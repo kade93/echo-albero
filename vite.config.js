26fecde2f3733c9dddfa7e61d57807823cdea62c
DONGHYUN YOUN <kade@dudaji.com>
2026-01-28 15:44:21 +0900

    fix: Update project name from echo-albero to eco-albero in configuration files.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/echo-albero/",
+    base: "/eco-albero/",
     server: {
         host: true,
     },
